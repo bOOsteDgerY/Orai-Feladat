725508b506726e09b63d8aa80191aefa2d2c5a10
Vidra Gergő <vidragery@gmail.com>
2022-12-12 14:19:03 +0100

    gery: v1

--- src/Main/main.java
+++ src/Main/main.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         Scanner scan = new Scanner(System.in);
         switch (scan.nextLine()){
+            case "hello": System.out.println("Hello World"); break;
             case "info":
-                System.out.println("Üdvözöllek, ez itt a v1-es projekt!"); break;
+                System.out.println("Üdvözöllek, ez itt a v2-es projekt!"); break;
             case "exit": System.exit(1); break;
+            case "help":
+                System.out.println("Parancsok: hello/info/help/exit"); break;
         }
     }
 }
